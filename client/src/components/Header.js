657803f14cd51b7446f5c95ac7dd84a09f367b41
Dipesh Sukhani <dipeshsukhani@gmail.com>
2017-12-08 01:48:51 +0800

    Header Component
    Worked on the Header.js file to apply the basic nav bar  code available on MaterializeCSS website.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,10 +3,15 @@ import React, {Component} from 'react';
 class Header extends Component {
   render() {
     return (
-      <div>
-      Header
+      <nav>
+      <div className="nav-wrapper">
+        <a href="#!" className="brand-logo">Emaily</a>
+        <ul className="right hide-on-med-and-down">
+          <li><a href="#!" class="active">Login With Google</a></li>
+          <li><a href="#!">Test2</a></li>
+        </ul>
       </div>
-    );
+    </nav>);
   }
 }
 export default Header;
